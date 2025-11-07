2e989c3db088dbc3e94d831e4fde0052b3505e1c
vickykumarjsr111-dot <vickykumarjsr111@gmail.com>
2025-11-07 21:50:31 +0530

    new chnages

--- password-generator/vite.config.js
+++ password-generator/vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js  (or vite.config.ts)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/password-generator/',      // for GitHub Pages: /<repo-name>/
-  build: { outDir: 'docs' } // output build to /docs
+  base: '/password-generator/',   // repo name
+  build: { outDir: 'docs' }       // build to /docs
 })
